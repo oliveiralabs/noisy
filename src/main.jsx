8e7a9e51b85d29b6cd1e7e5382be82515a3ec71a
Antônio Oliveira <antonio.filho128@gmail.com>
2023-02-08 23:12:37 -0300

    Firebase configuration

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,27 @@ import {
 } from "react-router-dom";
 import Home from './views/home'
 import SoundDetails from './views/sound-details'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAwtNdOMw-psg7e6Fv2Szr-Rk0bUlPJTc",
+  authDomain: "noisy-eb78c.firebaseapp.com",
+  projectId: "noisy-eb78c",
+  storageBucket: "noisy-eb78c.appspot.com",
+  messagingSenderId: "279966313384",
+  appId: "1:279966313384:web:e1d33644eeab2ea6a4cb9d",
+  measurementId: "G-2VZ0CZXB9L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const router = createHashRouter([
   {
